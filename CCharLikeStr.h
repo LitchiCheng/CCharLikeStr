b495ce0283a26bac150c5d1b83f55cab1e142197
darboy <974782852@qq.com>
2019-06-25 22:41:49 +0800

    构建类，头文件

---
+++ CCharLikeStr.h
@@ -0,0 +1,24 @@
+#ifndef _C_CHAR_LIKE_STR_H_
+#define _C_CHAR_LIKE_STR_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+class CCharLikeStr
+{
+private:
+    /* data */
+public:
+    CCharLikeStr(/* args */);
+    ~CCharLikeStr();
+};
+
+CCharLikeStr::CCharLikeStr(/* args */)
+{
+}
+
+CCharLikeStr::~CCharLikeStr()
+{
+}
+
+
+#endif //_C_CHAR_LIKE_STR_H_
